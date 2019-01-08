8e7d6a93c28cd630bd3d5454f8ba809c7bc0d03e
Andrew Lord <andrew.lord@appliedblockchain.com>
2019-01-08 21:45:27 +0000

    :sparkles: Get latest block status & refactor to send requests batched in one

--- src/advanced/getParityNetworkStatus.js
+++ src/advanced/getParityNetworkStatus.js
@@ -1,11 +1,23 @@
 const { getNodeStatus } = require('../helpers/parity')
+const style = require('../helpers/textStyle')
 
 async function getParityNetworkStatus() {
   const parity1 = await getNodeStatus('parity1:8545')
   const parity2 = await getNodeStatus('parity2:9545')
   const parity3 = await getNodeStatus('parity3:10545')
+  let networkStatus
 
-  console.log({ parity1, parity2, parity3 })
+  if(parity1.blockHash === parity2.blockHash &&
+    parity2.blockHash === parity3.blockHash)
+  {
+    networkStatus = style.primary('In Sync')
+  } else {
+    networkStatus = style.error('Forked')
+  }
+
+  console.log('\n')
+  console.log(`> Parity Node Status (${networkStatus}):\n\n`)
+  console.table({ parity1, parity2, parity3 })
 }
 
 module.exports = {
--- src/helpers/parity.js
+++ src/helpers/parity.js
@@ -1,23 +1,50 @@
 const axios = require('axios')
 
-async function getNodeStatus(node = 'parity1:8545') {
-  const { data: client } = await axios.post(`http://${node}`, {
-    jsonrpc: '2.0',
-    method: 'web3_clientVersion',
-    params: [],
-    id: 67
-  })
+function formatClientVersion(rawClientVersion) {
+  const regex = /v[0-9]+.[0-9]+.[0-9]+/g
+  const result = rawClientVersion.match(regex)
 
-  const { data: coinbaseAccount } = await axios.post(`http://${node}`, {
-    jsonrpc: '2.0',
-    method: 'eth_coinbase',
-    params: [],
-    id: 46
-  })
+  return  Array.isArray(result) ? result[0] : 'unknown'
+}
+
+async function getNodeStatus(node = 'parity1:8545') {
+  // batch request node status data
+  const { data: [ client, coinbaseAccount, blockNumber, block ] } = await axios.post(`http://${node}`, [
+    // parity client version on node
+    {
+      jsonrpc: '2.0',
+      method: 'web3_clientVersion',
+      params: [],
+      id: 67
+    },
+    // node admin account
+    {
+      jsonrpc: '2.0',
+      method: 'eth_coinbase',
+      params: [],
+      id: 46
+    },
+    // current latest block number
+    {
+      jsonrpc: '2.0',
+      method: 'eth_blockNumber',
+      params: [],
+      id: 1
+    },
+    // latest block hash
+    {
+      jsonrpc: '2.0',
+      method: 'eth_getBlockByNumber',
+      params: [ "latest", false ],
+      id: 1
+    }
+  ])
 
   return {
-    clientVersion: client.result,
-    adminAccount: coinbaseAccount.result
+    clientVersion: formatClientVersion(client.result),
+    adminAccount: coinbaseAccount.result,
+    blockNumber: parseInt(blockNumber.result, 16),
+    blockHash: block.result.hash || '(pending)'
   }
 }
 
